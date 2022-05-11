1058417111bed44a76d70bccd6ab628502d6c339
Chicibelito70 <Chicibelito07@gmail.com>
2022-05-10 22:42:48 -0400

    Version 1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+  <label>
+    <input type="number" name="name" placeholder='Size' id='Sizeipt'/>
+  </label>
+  <input type="submit" value="Click" id='btnclick' />
+</form>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Logic.js
@@ -0,0 +1,28 @@
+import React from "react";
+import App from "./App";
+
+
+///combetir el input a texto 
+// 
+
+let med;
+function medida(){
+if (med=>160 && med<=165) {
+    alert("Su talla de bici es XS")
+}
+if(med=>166  &&med<=170){
+    alert("Su talla de bici es S")
+}
+if(med=>171  &&med<=179){
+    alert("Su talla de bici es M")
+}
+if(med=>180  &&med<=186){
+    alert("Su talla de bici es L")
+}
+if(med=>187  &&med<=195){
+    alert("Su talla de bici es XL")
+    return med
+}
+
+}
+
